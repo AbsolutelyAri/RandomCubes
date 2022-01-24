53e9c03d1b2440f60a530a0bd60ea85f249b61ad
Krieger <benjaminokrieger@icloud.com>
2022-01-24 16:34:53 -0600

    Created CubePrefab and RandomCubes script
    the script doesn't do anything yet but just you wait!

---
+++ RandomCubesUnity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/*
+    Created by: Krieger
+    Date Created: 1/24/2022
+
+    Last Edited by: n/a
+    Last Edited Date: 1/24/2021
+
+    Description: Spawns CubePrefab's into the scene.
+*/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
